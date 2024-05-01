58d2568ea734944a87df6311d82ec63971cde994
Zelalem Mekonnen <zelliott33@gmail.com>
2024-05-01 17:34:12 -0500

    video plays, touch reaction next

---
+++ app/src/main/java/com/pixelpeak/adpak/MainActivity.java
@@ -0,0 +1,39 @@
+package com.pixelpeak.adpak;
+
+import android.content.res.AssetFileDescriptor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        VideoView initialVideo = (VideoView) findViewById(R.id.initialVideo);
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.initial_video);
+        initialVideo.setVideoURI(videoUri);
+        initialVideo.start();
+        initialVideo.setOnCompletionListener(mediaPlayer -> {
+            initialVideo.start();
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+    }
+}
